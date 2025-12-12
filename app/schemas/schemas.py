d5793d1978b67614b67b97a1e1134a5bbb07fcd5
Frank Chen <frankchen120@outlook.com>
2025-12-12 11:09:58 +0800

    discount repo

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -8,13 +8,4 @@ class Item(BaseModel):
 class ItemResponse(BaseModel):
     name: str
     total: float
-
-class Discount(BaseModel):
-    name: str
-    price: int
-    discount: float
     
-class DiscountResponse(BaseModel):
-    name: str
-    final_price: float
-            
