24f96d74adb8e4d183b263da799bdcc936fbc879
Frank Chen <frankchen120@outlook.com>
2025-12-12 22:10:06 +0800

    error detail

--- app/routers/discount.py
+++ app/routers/discount.py
@@ -27,7 +27,10 @@ def create(discount: Discount, db: Session = Depends(get_db)):
     except ValueError as e:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=str(e)
+            detail={
+                "code": "INVALUD_DISCOUNT",
+                "message": str(e)
+            }
         )
         
 
