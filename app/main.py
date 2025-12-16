bbf0d032fa1929ad897dd8739e515f05be9501e7
Frank Chen <frankchen120@outlook.com>
2025-12-16 11:34:54 +0800

    include auth router

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from app.routers.discount import router as discount_router
+from app.routers.auth import router as auth_router
 from app.routers.order import router as order_router
 from app.routers.report import router as report_router
 from app.core.logging import setup_logging
@@ -70,5 +71,6 @@ def create_item(item: schemas.Item):
 
 
 app.include_router(discount_router)
+app.include_router(auth_router)
 app.include_router(order_router)
 app.include_router(report_router)
