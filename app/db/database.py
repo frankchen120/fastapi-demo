e103590906ee602ba5f7889dd66de3ec711b4b6a
Frank Chen <frankchen120@outlook.com>
2025-12-17 15:23:11 +0800

    database with env

--- app/db/database.py
+++ app/db/database.py
@@ -1,9 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg2://app:app123@localhost:5432/fastapi_db"
+# DATABASE_URL = "postgresql+psycopg2://app:app123@localhost:5432/fastapi_db"
+DATABASE_URL = (
+    f"postgresql+psycopg2://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}"
+    f"@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
+)
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
