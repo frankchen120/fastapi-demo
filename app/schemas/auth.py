efee3c4e0f1f17dd183f4038404590e615fa9069
Frank Chen <frankchen120@outlook.com>
2025-12-20 11:07:46 +0800

    validate password

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -1,8 +1,16 @@
 from pydantic import BaseModel, EmailStr
+from pydantic import field_validator
 
 class RegisterRequest(BaseModel):
     email: EmailStr
     password: str
+    
+    @field_validator("password")
+    @classmethod
+    def password_length(cls, v:str):
+        if len(v) < 8:
+            raise ValueError("password must be at least 8 characters")
+        return v
 
 class LoginRequest(BaseModel):
     email: EmailStr
